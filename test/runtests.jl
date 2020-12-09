2c5543ab9a53a17ff869e7cab7c60d1d07de946a
Marius Pille <marius.pille@hu-berlin.de>
2020-12-09 14:15:05 +0100

    Generalize runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,11 @@ using Cardio
 using Test
 using Random
 
-testfiles = ["test_adaptive_hrv_filter.jl", "test_detectRPeaks.jl", "test_util.jl"]
-
 Random.seed!(1776)
 
-for testfile in testfiles
+#testfiles = ["test_adaptive_hrv_filter.jl", "test_detectRPeaks.jl", "test_util.jl"]
+(root, dirs, files) = first(walkdir(dirname(@__FILE__)))
+
+for testfile in files[occursin.("test_", files)]
     eval(:(@testset $testfile begin include($testfile) end))
 end
