1d012c117d4fa193854576c78e41f0ac872629d2
marius@dirk <marius.pille@hu-berlin.de>
2021-07-27 17:11:15 +0200

    add try catch

--- src/BRS/arBRS.jl
+++ src/BRS/arBRS.jl
@@ -28,8 +28,17 @@ function arbrs(RR::Vector{<:Real}, SBP::Vector{<:Real}; p::Union{Int, UnitRange{
     HFSBP = getSpectralComponent(SSBP, centerFrequenciesSBP, HF)
     fullSBP = getSpectralComponent(SSBP, centerFrequenciesSBP)
 
-    αLF = sqrt(sum(LFRR) / sum(LFSBP))
-    αHF = sqrt(sum(HFRR) / sum(HFSBP))
+    try # something became negative here
+        αLF = sqrt(sum(LFRR) / sum(LFSBP))
+    catch
+        αLF = NaN
+    end
+
+    try
+        αHF = sqrt(sum(HFRR) / sum(HFSBP))
+    catch
+        αHF = NaN
+    end
 
     return arBRS(αLF = αLF, αHF = αHF, LFRR = LFRR, LFSBP = LFSBP, HFRR = HFRR, HFSBP = HFSBP, f = collect(f), fullRR = fullRR, fullSBP = fullSBP)
 end
